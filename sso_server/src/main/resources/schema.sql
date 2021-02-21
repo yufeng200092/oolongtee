4abcdfa073fb278275ae3c0a17e7b7986659b321
yf <yufeng200092@qq.com>
2021-02-21 23:36:23 +0800

    add spring security dependency

---
+++ sso_server/src/main/resources/schema.sql
@@ -0,0 +1,15 @@
+create schema sso;
+use sso;
+CREATE TABLE `sso`.`oauth_client_details` (
+  `client_id` INT NOT NULL AUTO_INCREMENT,
+  `client_secret` VARCHAR(45) NOT NULL,
+  `resource_ids` VARCHAR(200) NULL,
+  `scope` VARCHAR(45) NULL,
+  `authorized_grant_types` VARCHAR(45) NULL,
+  `web_server_redirect_uri` VARCHAR(1000) NULL,
+  `authorities` VARCHAR(100) NULL,
+  `access_token_validity` INT NULL,
+  `refresh_token_validity` INT NULL,
+  `additional_information` VARCHAR(200) NULL,
+  `autoapprove` BIT NULL,
+  PRIMARY KEY (`client_id`));
