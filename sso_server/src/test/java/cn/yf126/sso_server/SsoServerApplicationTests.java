a14e02c367a56a2cadfaa44c34cdf78c375ec118
yf <yufeng200092@qq.com>
2021-02-21 21:05:16 +0800

    init

---
+++ sso_server/src/test/java/cn/yf126/sso_server/SsoServerApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.yf126.sso_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SsoServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
